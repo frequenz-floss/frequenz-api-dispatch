c74aa0f9b7d8af38d5f769f18ec1fa5ab8f2c96f
Joseph Rushton Wakeling <joseph.wakeling@frequenz.com>
2022-01-11 18:33:27 +0100

    Add dispatch database schema
    This is designed to allow cloud-side decision-makers to request dispatch
    of resources from individual edge locations.
    
    Dispatch is described in terms of the type of resource to be activated,
    in a specific location, over a specific time interval.  The status and
    settings of this dispatch may change many times, but this triple setting
    of `(location_id, resource_id, time_interval_id)` forms a unique ID for
    the dispatch entity itself.
    
    The schema is designed to be purely additive, i.e. once created each row
    can/should be treated as immutable.  Dispatch status and settings can be
    updated by writing a new `dispatch_modification` row, the most recent of
    which (by `create_time`) determines the current values.
    
    `location_id` values are treated purely as numbers in this schema: it is
    assumed that they are defined (and linked e.g. to enterprises, TSO/DSOs,
    etc.) in some other database.

---
+++ sql/create.sql
@@ -0,0 +1,100 @@
+/*******************************************************************************
+
+    (Re)create database (uncomment if needed)
+
+*******************************************************************************/
+
+/*
+DROP DATABASE IF EXISTS dispatch;
+
+CREATE DATABASE dispatch
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION_LIMIT = -1;
+*/
+
+\c dispatch
+
+CREATE SCHEMA IF NOT EXISTS dispatch
+    AUTHORIZATION postgres;
+
+
+/*******************************************************************************
+
+    Custom domains and types used in the schema
+
+*******************************************************************************/
+
+CREATE TYPE dispatch."dispatch_status" AS ENUM (
+    'ACTIVE',
+    'INACTIVE',
+    'SIMULATED'
+);
+
+
+--- When dispatch takes place
+CREATE TABLE dispatch."time_interval"(
+    "id" BIGSERIAL NOT NULL,
+    "start_time" TIMESTAMP(0) WITH TIME zone NOT NULL,
+    "end_time" TIMESTAMP(0) WITH TIME zone NOT NULL
+);
+ALTER TABLE dispatch."time_interval"
+    ADD PRIMARY KEY("id");
+CREATE UNIQUE INDEX "time_interval_unique_index"
+    ON dispatch."time_interval"("start_time", "end_time");
+
+
+--- Individual types of resource to dispatch
+CREATE TABLE dispatch."resource"(
+    "id" BIGSERIAL NOT NULL,
+    "type" VARCHAR(64) NOT NULL
+);
+ALTER TABLE dispatch."resource"
+    ADD PRIMARY KEY("id");
+CREATE UNIQUE INDEX "resource_unique_index"
+    ON dispatch."resource"("type");
+
+
+--- Individual dispatches for specific locations and time intervals
+CREATE TABLE dispatch."dispatch"(
+    "id" BIGSERIAL NOT NULL,
+    "location_id" BIGINT NOT NULL,
+    "resource_id" BIGINT NOT NULL,
+    "time_interval_id" BIGINT NOT NULL
+);
+ALTER TABLE dispatch."dispatch"
+    ADD PRIMARY KEY("id");
+ALTER TABLE dispatch."dispatch"
+    ADD CONSTRAINT "dispatch_resource_id_foreign"
+        FOREIGN KEY("resource_id")
+        REFERENCES dispatch."resource"("id");
+ALTER TABLE dispatch."dispatch"
+    ADD CONSTRAINT "dispatch_time_interval_id_foreign"
+        FOREIGN KEY("time_interval_id")
+        REFERENCES dispatch."time_interval"("id");
+CREATE UNIQUE INDEX "dispatch_unique_index"
+    ON dispatch."dispatch"(
+        "location_id",
+        "resource_id",
+        "time_interval_id"
+    );
+
+
+--- Modifications to the settings for individual dispatches
+CREATE TABLE dispatch."dispatch_modification"(
+    "id" BIGSERIAL NOT NULL,
+    "create_time" TIMESTAMP WITH TIME zone NOT NULL DEFAULT current_timestamp,
+    "dispatch_id" BIGINT NOT NULL,
+    "status" dispatch."dispatch_status" NOT NULL,
+    "settings" JSON NULL  -- contents determined by dispatch type
+);
+ALTER TABLE dispatch."dispatch_modification"
+    ADD PRIMARY KEY("id");
+ALTER TABLE dispatch."dispatch_modification"
+    ADD CONSTRAINT "dispatch_modification_dispatch_id_foreign"
+        FOREIGN KEY("dispatch_id")
+        REFERENCES dispatch."dispatch"("id");
