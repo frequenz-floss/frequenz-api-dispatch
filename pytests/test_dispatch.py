f35696bd79d4c885bdbf1d73224d57a2a39aa6ee
Leandro Lucarella <leandro.lucarella@frequenz.com>
2023-04-13 16:46:24 +0200

    Fix pylint issues
    This means mainly documenting the test file and functions, but we also
    add the copyright notice and license while we are at it.
    
    Signed-off-by: Leandro Lucarella <leandro.lucarella@frequenz.com>

--- pytests/test_dispatch.py
+++ pytests/test_dispatch.py
@@ -1,14 +1,25 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Basic tests to check some code is generated."""
+
+
 def test_package_import() -> None:
-    import frequenz.api.dispatch as dispatch
+    """Test that the package can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api import dispatch
 
     assert dispatch is not None
 
 
 def test_module_import() -> None:
-    import frequenz.api.dispatch.dispatch_pb2 as dispatch_pb2
+    """Test that the modules can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.dispatch import dispatch_pb2
 
     assert dispatch_pb2 is not None
 
-    import frequenz.api.dispatch.dispatch_pb2_grpc as dispatch_pb2_grpc
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.dispatch import dispatch_pb2_grpc
 
     assert dispatch_pb2_grpc is not None
